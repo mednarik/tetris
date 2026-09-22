37b7889840cd511147dbad7b0eb49ac8f93000aa
SchulLaptop <michi.bednarik@gmail.com>
2026-09-22 14:14:34 +0200

    restructure

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ def figures_will_overlap(fig1, direction, figures): #check if a figure overlaps 
         y_modifier = 1
     
     for figure in figures: #get all occupied positions
-        for i in range(figure.size):
-            figures_x_positions.append(figure.x + i)
-            figures_y_positions.append(figure.y + i)
+        for i in range(2):
+            figures_x_positions.append(figure[0] + i)
+            figures_y_positions.append(figure[1] + i)
     for i in range(fig1.size):  #for every size part
          if fig1.x + i + x_modifier in figures_x_positions and fig1.y + y_modifier + i in figures_y_positions:
             return True
@@ -29,6 +29,7 @@ def figures_will_overlap(fig1, direction, figures): #check if a figure overlaps 
 class Figure():
     def __init__(self, pos: tuple):
         self.size = 2
+        self.spawnpos = pos
         self.x = pos[0]
         self.y = pos[1]
 
@@ -50,19 +51,27 @@ class Figure():
 
         return False #it didn't move
 
-
+    def reset(self):
+        self.x = self.spawnpos[0]
+        self.y = self.spawnpos[1]
+        
 class Board():
     def __init__(self):
         self.width = 10
         self.height = 16
-    def draw_board(self, figures):
+    def draw_board(self, active, placed_positions):
+
         for i in range(self.height): #for every row
             figs_x_positions = [] #list for every occupied x position in this row
-            for figure in figures: #check for every figure
-                if figure.y == i or figure.y + 1 == i: #if it is on that row
-                    for i in range(figure.size): #add its x positions to the list
-                        figs_x_positions.append(figure.x + i)
-            
+            if active.y == i or active.y + 1 == i: #if it is on that row
+                for i in range(active.size): #add its x positions to the list
+                    figs_x_positions.append(active.x + i )
+
+            for pos in placed_positions:
+                if pos[1] or pos[1] - 1 == i:
+                    figs_x_positions.append(pos[0])
+                    figs_x_positions.append(pos[0] + 1)
+                
             for i in range(self.width):
                 if i in figs_x_positions:
                     print(figure_symbol, end="")
@@ -73,22 +82,22 @@ class Board():
 
 def main():
     board = Board()
-    placed_figures = []
-    figures = []
+    placed_positions = []
+    fig1 = Figure((4, 0))
 
     while True:
-        fig1 = Figure((4, 0))
-        figures.append(fig1)
+        
         alive = True
         while alive:
             
-            board.draw_board(figures)
+            board.draw_board(fig1, placed_positions)
 
-            alive = fig1.gravity(board.height, placed_figures)
+            alive = fig1.gravity(board.height, placed_positions)
             if not alive:
-                placed_figures.append(fig1)
+                placed_positions.append((fig1.x, fig1.y))
+                fig1.reset()
 
-            fig1.move(input("Where do you want to go? (a/d): "), board.width, placed_figures)
+            fig1.move(input("Where do you want to go? (a/d): "), board.width, placed_positions)
             os.system("cls")
 def test():
     fig1 = Figure((4, 0))
