616b01c71ab674a1d435329ee2fc290736ae3544
Michael Bednarik <michi.bednarik@gmail.com>
2026-09-23 23:17:58 +0200

    changed logic of  storing placed stuff

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os, time
 
-bg_symbol = "."
-figure_symbol = "#"
+bg_char = '.'
+figure_char = '#'
 ########################################################
-def is_row_full(figure_positions, board_width, board_height):
+def is_row_full(placed_positions, board_width, board_height):
 
     example = [i for i in range(0, board_width, 2)]
-    y_positions = [pos[0] for pos in figure_positions if pos[1] == board_height - 2]
+    y_positions = [pos[0] for pos in placed_positions]
     if y_positions == example:
         return True
     return False
@@ -15,7 +15,17 @@ def remove_full_line(figure_positions, board_height):
     lst = [(pos[0], pos[1] + 1) for pos in figure_positions if pos[1] + 1 < board_height]
     return lst
 ##################################################
-def figures_will_overlap(fig, direction, figures): #check if a figure overlaps with any figure in a given list
+
+
+def join_two_lists(lst1, lst2):
+    lst = []
+    for item in lst1:
+        lst.append(item)
+    for item in lst2:
+        lst.append(item)
+    return lst
+
+def squares_will_overlap(alpha_sigma_lone_wolf_ahh_square, direction, squares): #check if a square overlaps with any other square in a given list
     
     if direction == "right":
         x_modifier = 1
@@ -27,17 +37,9 @@ def figures_will_overlap(fig, direction, figures): #check if a figure overlaps w
         x_modifier = 0
         y_modifier = 1
 
-    figures_positions = []
-    for figure in figures:
-        for i in range(2):
-            for j in range(2):
-                figures_positions.append((figure[0] + i, figure[1] + j)) #get occupied positions
-
-    
-    for i in range(2):  #for every size part
-        for j in range(2):
-            if (fig.x + x_modifier + i, fig.y + y_modifier + j) in figures_positions:
-                return True
+    for square in squares:
+        if (alpha_sigma_lone_wolf_ahh_square[0] + x_modifier, alpha_sigma_lone_wolf_ahh_square[1] + y_modifier) == square:
+            return True
     return False
 
 
@@ -48,8 +50,13 @@ class Figure():
         self.x = pos[0]
         self.y = pos[1]
 
-    def gravity(self, board_height, figures):
-        if self.y + self.size < board_height and not figures_will_overlap(self, "down", figures):
+    def gravity(self, board_height, placed_squares):
+        way_blocked = False
+        for square in self.get_occupied_squares():
+            if squares_will_overlap(square, "down", placed_squares):
+                way_blocked = True
+                break        
+        if self.y + self.size < board_height and not way_blocked:
             self.y += 1
             if self.y + self.size == board_height:
                 return False #it was able to fall but now its on the bottom
@@ -57,19 +64,35 @@ class Figure():
                 return True #it was able to fall
         return False #it wasn't able to fall
     
-    def move(self, direction: str, board_width: int, figures: list):
+    def move(self, direction: str, board_width: int, placed_squares: list):
         if direction == "a":
             direction = "left"
-            if self.x > 0 and not figures_will_overlap(self, direction, figures):
+            way_blocked = False
+            for square in self.get_occupied_squares():
+                if squares_will_overlap(square, direction, placed_squares):
+                    way_blocked = True
+                    break
+            if self.x > 0 and not way_blocked:
                 self.x -= 1
                 return True #it moved
         elif direction == "d":
             direction = "right"
-            if self.x + self.size < board_width and not figures_will_overlap(self, direction, figures):
+            way_blocked = False
+            for square in self.get_occupied_squares():
+                if squares_will_overlap(square, direction, placed_squares):
+                    way_blocked = True
+                    break
+            if self.x + self.size < board_width and not way_blocked:
                 self.x += 1
                 return True #it moved
-
         return False #it didn't move
+    
+    def get_occupied_squares(self):
+        occupied_squares = []
+        for i in range(self.size):
+            for j in range(self.size):
+                occupied_squares.append((self.x + i, self.y + j))
+        return occupied_squares
 
     def reset(self):
         self.x = self.spawnpos[0]
@@ -79,25 +102,15 @@ class Board():
     def __init__(self):
         self.width = 10
         self.height = 16
-    def draw_board(self, active, placed_positions):
+    def draw_board(self, positions):
 
-        for i in range(self.height): #for every row
-            figs_x_positions = [] #list for every occupied x position in this row
-            if active.y == i or active.y + 1 == i: #if it is on that row
-                for j in range(active.size): #add its x positions to the list
-                    figs_x_positions.append(active.x + j )
-
-            for pos in placed_positions:
-                if pos[1] == i or pos[1] + 1 == i:
-                    figs_x_positions.append(pos[0])
-                    figs_x_positions.append(pos[0] + 1)
-                
-            for j in range(self.width):
-                if j in figs_x_positions:
-                    print(figure_symbol, end="")
+        for y in range(self.height):
+            for x in range(self.width):
+                if (x, y) in positions:
+                    print(figure_char, end="")
                 else:
-                    print(bg_symbol, end="")
-            print() # go to a new row
+                    print(bg_char, end="")
+            print()
 
 
 def main():
@@ -111,28 +124,28 @@ def main():
         while alive:
 
             os.system("cls")
-            board.draw_board(fig1, placed_positions)
+            board.draw_board(join_two_lists(placed_positions, fig1.get_occupied_squares()))
     
             
 
             fig1.move(input("Where do you want to go? (a/d): "), board.width, placed_positions)
 
             os.system("cls")
-            board.draw_board(fig1, placed_positions)
+            board.draw_board(join_two_lists(placed_positions, fig1.get_occupied_squares()))
+
         
 
             alive = fig1.gravity(board.height, placed_positions)
 
             if not alive:
-                placed_positions.append((fig1.x, fig1.y))
+                for square in fig1.get_occupied_squares():
+                    placed_positions.append(square)
                 fig1.reset()
 
-            if is_row_full(placed_positions, board.width, board.height):
-                placed_positions = remove_full_line(placed_positions, board.height)
+            #if is_row_full(placed_positions, board.width, board.height):
+                #placed_positions = remove_full_line(placed_positions, board.height)
 
 def test():
-    lst = [(i, 16) for i in range(10)]
-    print(lst)
-    print(is_row_full(lst, 10, 17))
-
+    fig1 = Figure((1, 1))
+    print(fig1.get_occupied_squares())
 main()
